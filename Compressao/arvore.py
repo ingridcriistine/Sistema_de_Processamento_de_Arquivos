6908ad1ec9da11e7cd999066b3a9590ccd79ac75
milena579 <milecalegari@gmail.com>
2025-11-21 19:21:11 -0300

    começando compressao de huffman

---
+++ Compressao/arvore.py
@@ -0,0 +1,23 @@
+from fila_prioridade import FilaDePrioridadeManual
+from node import Node
+
+def construir_arvore_huffman(freq_map):
+    pq = FilaDePrioridadeManual()
+
+    for char, freq in freq_map.items():
+        pq.add(Node(char=char, frequency=freq))
+
+    while pq.size() > 1:
+        n1 = pq.poll()
+        n2 = pq.poll()
+
+        novo = Node(
+            char=None,
+            frequency=n1.frequency + n2.frequency,
+            left=n1,
+            right=n2
+        )
+
+        pq.add(novo)
+
+    return pq.poll()  # raiz da árvore
---
+++ Compressao/compactador.py
@@ -0,0 +1,27 @@
+from lendo_arquivo import PrimeiraLeituraStreaming
+from lendo_arquivo import SegundaLeituraStreaming
+from arvore import construir_arvore_huffman
+from codigos import gerar_codigos
+
+def huffman_compactar(entrada, saida):
+    leitor1 = PrimeiraLeituraStreaming(entrada)
+    leitor1.processar()
+
+    root = construir_arvore_huffman(leitor1.freq)
+    codigos = gerar_codigos(root)
+
+    print("Códigos de Huffman gerados:")
+    for char, code in codigos.items():
+        if char == "\n":
+            print("\\n:", code)
+        elif char == " ":
+            print("' ':", code)
+        else:
+            print(f"{char}: {code}")
+
+
+    leitor2 = SegundaLeituraStreaming(entrada, codigos)
+    leitor2.comprimir_para_binario(saida)
+
+    print("Arquivo compactado com sucesso!")
+
