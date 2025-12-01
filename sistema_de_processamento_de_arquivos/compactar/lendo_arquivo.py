b6cfc1a4a713c267d20eff5c2ee7597fb9b75cc3
ingridcriistine <ingridrochacristine@gmail.com>
2025-11-30 21:45:37 -0300

    ajustes

--- sistema_de_processamento_de_arquivos/compactar/lendo_arquivo.py
+++ sistema_de_processamento_de_arquivos/compactar/lendo_arquivo.py
@@ -70,27 +70,22 @@ class SegundaLeituraStreaming:
 
     def comprimir_para_blocos(self, saida):
 
-        # --------- TABELA HUFFMAN (igual ao seu) ---------
         tabela_json = json.dumps(self.codigos).encode("utf-8")
         tamanho_tabela = len(tabela_json)
 
         with open(saida, "wb") as f_out:
 
-            # Grava primeira tabela geral
             f_out.write(tamanho_tabela.to_bytes(4, "big"))
             f_out.write(tabela_json)
-
-            # --------- INÍCIO DAS LEITURAS POR BLOCO ---------
+            
             with open(self.caminho, "r", encoding="utf-8") as f_in:
 
-                global_pos = 0  # posição absoluta no texto
-
+                global_pos = 0  
                 while True:
                     bloco = f_in.read(self.block_size)
                     if not bloco:
                         break
 
-                    # --------- Compressão do bloco ---------
                     buffer_bits = ""
                     bloco_bytes = bytearray()
 
@@ -102,23 +97,18 @@ class SegundaLeituraStreaming:
                             bloco_bytes.append(byte)
                             buffer_bits = buffer_bits[8:]
 
-                    # padding final
                     padding = 0
                     if buffer_bits:
                         padding = 8 - len(buffer_bits)
                         buffer_bits = buffer_bits.ljust(8, "0")
                         bloco_bytes.append(int(buffer_bits, 2))
 
-                    # --------- GRAVA O BLOCO ---------
                     bloco_size = len(bloco_bytes)
 
-                    # 1) Tamanho do bloco
                     f_out.write(bloco_size.to_bytes(4, "big"))
 
-                    # 2) Bloco comprimido
                     f_out.write(bytes(bloco_bytes))
 
-                    # --------- CRIA ÍNDICE DO BLOCO ---------
                     indice = {
                         "start_global": global_pos,
                         "end_global": global_pos + len(bloco) - 1,
@@ -128,11 +118,8 @@ class SegundaLeituraStreaming:
                     indice_json = json.dumps(indice).encode("utf-8")
                     indice_size = len(indice_json)
 
-                    # 3) tamanho do índice
                     f_out.write(indice_size.to_bytes(4, "big"))
 
-                    # 4) índice em si
                     f_out.write(indice_json)
 
-                    # Atualiza posição global
                     global_pos += len(bloco)
